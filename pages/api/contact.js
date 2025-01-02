998b59eb658152a7be7ec8adee146ebf467d701d
DruxAMB <druxambmarketing@gmail.com>
2025-01-02 11:24:57 +0100

    installed nodemailer so as to receive mails from clients

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,8 +1,13 @@
-const { Client } = require('@notionhq/client');
+const nodemailer = require('nodemailer');
 
-const notion = new Client({
-  auth: process.env.NOTION_API_TOKEN,
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASS,
+  },
 });
+
 
 export default async (req, res) => {
   if (req.method !== 'POST') {
@@ -10,43 +15,16 @@ export default async (req, res) => {
   }
   try {
     const { name, email, subject, message } = JSON.parse(req.body);
-    await notion.pages.create({
-      parent: {
-        database_id: process.env.NOTION_DATABASE_ID,
-      },
-      properties: {
-        Name: {
-          title: [
-            {
-              text: {
-                content: name,
-              },
-            },
-          ],
-        },
-        Email: {
-          email,
-        },
-        Subject: {
-          rich_text: [
-            {
-              text: {
-                content: subject,
-              },
-            },
-          ],
-        },
-        Message: {
-          rich_text: [
-            {
-              text: {
-                content: message,
-              },
-            },
-          ],
-        },
-      },
-    });
+
+    const mailOptions = {
+      from: email,
+      to: 'druxambsite@gmail.com',
+      subject: `Contact Form Submission: ${subject}`,
+      text: `Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
     res.status(201).json({ msg: 'Success' });
   } catch (error) {
     console.log(error);
--- pages/resume.jsx
+++ pages/resume.jsx
@@ -1,4 +1,4 @@
-import styles from "../styles/ContactPage.module.css";
+import styles from "../styles/ResumePage.module.css";
 
 const ContactPage = () => {
   return (
